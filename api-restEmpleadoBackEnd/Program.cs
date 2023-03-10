cec20921827b730a1c3ba4924b21a770c5765174
AntonioValentinSayago <cesar_anto30@hotmail.com>
2023-03-09 20:55:15 -0600

    Creacion del DTO

---
+++ api-restEmpleadoBackEnd/Program.cs
@@ -0,0 +1,63 @@
+using api_restEmpleadoBackEnd.Models;
+using Microsoft.EntityFrameworkCore;
+
+using api_restEmpleadoBackEnd.Services.Contrato;
+using api_restEmpleadoBackEnd.Services.Implementacion;
+
+using AutoMapper;
+using api_restEmpleadoBackEnd.DTOs;
+using api_restEmpleadoBackEnd.Utilidades;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Cadena de Conexion
+builder.Services.AddDbContext<ApiRestEmpleadoContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("cadenaSQL"));
+});
+
+builder.Services.AddScoped<IDepartamentoService, DepartamentoService>();
+builder.Services.AddScoped<IEmpleadoService, EmpleadoService>();
+
+builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateTime.Now.AddDays(index),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ api-restEmpleadoBackEnd/Services/Contrato/IEmpleadoService.cs
@@ -0,0 +1,13 @@
+﻿using api_restEmpleadoBackEnd.Models;
+
+namespace api_restEmpleadoBackEnd.Services.Contrato
+{
+    public interface IEmpleadoService
+    {
+        Task<List<Empleado>> GetList();
+        Task<Empleado> Get(int idEmpleado);
+        Task<Empleado> Add(Empleado modelo);
+        Task<bool> Update(int idEmpleado);
+        Task<bool> Delete(int idEmpleado);
+    }
+}
